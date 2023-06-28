4ce00dcc7824f4a639d4eeb1c8916bc212d46ac2
niekky <trungkiencfc@gmail.com>
2023-06-28 22:28:42 +0700

    Redesign Project Showcase

--- src/pages/application/index.tsx
+++ src/pages/application/index.tsx
@@ -4,13 +4,22 @@ import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const isBrowser = () => typeof window !== 'undefined';
+
+
+function scrollToProject(){
+  if (!isBrowser()) return;
+  window.scrollTo({ top: 730, behavior: 'smooth' });
+}
+
 export default function Home() {
   return (
     <div className='Parent'>
-      <div className='flex-col w-full h-screen bg-gray-600'>
-        <div className='flex-col w-full h-2/4 bg-yellow-500' id='intro_background'>
+      <div className='flex-col w-full bg-gray-600'>
+        
+        <div className='flex-col w-full bg-yellow-500' id='intro_background'>
           <nav id="top_bar">
-            <div className='justify-between px-20 py-6 hidden w-full md:block md:w-auto'>
+            <div className='justify-between px-24 py-6 hidden w-full md:block md:w-auto'>
               <ul className="flex justify-end font-normal text-2xl flex flex-col md:flex-row md:space-x-12">
                 <li>
                   <a href='/' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</a>
@@ -24,76 +33,63 @@ export default function Home() {
               </ul>
             </div>
           </nav>
-          <p className='flex font-normal text-6xl px-24 py-28'>Software Application</p>
+          <p className='flex font-medium text-6xl px-48 py-36'>Software Application</p>
         </div>
         
-        <div className='flex-col w-full h-1/3 bg-gray-700 px-24 py-12 space-y-4' id='detail'>
+        <div className='flex-col w-full h-fit bg-gray-700 px-48 py-24 space-y-4' id='detail'>
           <p className='flex font-medium text-3xl'>Overview</p>
           <p className='flex font-thin text-lg w-2/3'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
         </div>
        
-        {/* <div className='flex-col w-full bg-gray-800 px-48' id='projects'>
-          <div className='flex w-full h-1/2' id="project_layout">
-            <div className='flex-col w-1/2 space-y-5' id='description'>
-              <p className='flex font-normal text-5xl'>Title</p>
-              <p className='flex font-thin text-xl'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-            </div>
-            <div className='flex-col w-1/2 bg-yellow-500' id="image">
-              <p className='flex justify-center items-center'>Picture</p>
+        <div className='flex-col w-full h-fit bg-gray-800'>
+          <button onClick={scrollToProject} className='flex w-full font-thin justify-center text-3xl p-12 hover:font-normal'>Projects ↓</button>
+          <div className='flex w-full h-fit'>
+            <div className='grid grid-cols-2 w-full bg-gray-900 mx-48 mt-12 mb-32 py-24 px-32 gap-x-16 gap-y-24'>
+              <div className='flex-col h-[420px] '>
+                <div className='flex bg-yellow-500 w-full h-5/6 hover:bg-opacity-50' id='project_img'>
+                  <div className='flex-col w-full h-full p-12 space-y-3 opacity-0 hover:opacity-100' id='project_desc'>
+                    <p className='text-2xl font-normal'>DescriptionName:</p>
+                    <p className='font-thin'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
+                    {/* <Link href={""} className='hover:font-medium'>Status: Null</Link> */}
+                  </div>
+                </div>
+                <p className='flex h-1/6 w-full text-center justify-center text-xl items-center' id='project_name'>Description</p>
+              </div>
+              <div className='flex-col h-[420px] '>
+                <div className='flex bg-yellow-500 w-full h-5/6 hover:bg-opacity-50' id='project_img'>
+                  <div className='flex-col w-full h-full p-12 space-y-3 opacity-0 hover:opacity-100' id='project_desc'>
+                    <p className='text-2xl font-normal'>DescriptionName:</p>
+                    <p className='font-thin'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
+                    {/* <Link href={""} className='hover:font-medium'>Status: Null</Link> */}
+                  </div>
+                </div>
+                <p className='flex h-1/6 w-full text-center justify-center text-xl items-center' id='project_name'>Description</p>
+              </div>
+              <div className='flex-col h-[420px] '>
+                <div className='flex bg-yellow-500 w-full h-5/6 hover:bg-opacity-50' id='project_img'>
+                  <div className='flex-col w-full h-full p-12 space-y-3 opacity-0 hover:opacity-100' id='project_desc'>
+                    <p className='text-2xl font-normal'>DescriptionName:</p>
+                    <p className='font-thin'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
+                    {/* <Link href={""} className='hover:font-medium'>Status: Null</Link> */}
+                  </div>
+                </div>
+                <p className='flex h-1/6 w-full text-center justify-center text-xl items-center' id='project_name'>Description</p>
+              </div>
+              <div className='flex-col h-[420px] '>
+                <div className='flex bg-yellow-500 w-full h-5/6 hover:bg-opacity-50' id='project_img'>
+                  <div className='flex-col w-full h-full p-12 space-y-3 opacity-0 hover:opacity-100' id='project_desc'>
+                    <p className='text-2xl font-normal'>DescriptionName:</p>
+                    <p className='font-thin'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
+                    {/* <Link href={""} className='hover:font-medium'>Status: Null</Link> */}
+                  </div>
+                </div>
+                <p className='flex h-1/6 w-full text-center justify-center text-xl items-center' id='project_name'>Description</p>
+              </div>
             </div>
           </div>
           
-          <div className='flex w-full h-1/2' id="project_layout">
-            <div className='flex-col w-1/2 space-y-5' id='description'>
-              <p className='flex font-normal text-5xl'>Title</p>
-              <p className='flex font-thin text-xl'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-            </div>
-            <div className='flex-col w-1/2 bg-yellow-500' id="image">
-              <p className='flex justify-center items-center'>Picture</p>
-            </div>
-          </div>
-
-          <div className='flex w-full h-1/2' id="project_layout">
-            <div className='flex-col w-1/2 space-y-5' id='description'>
-              <p className='flex font-normal text-5xl'>Title</p>
-              <p className='flex font-thin text-xl'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-            </div>
-            <div className='flex-col w-1/2 bg-yellow-500' id="image">
-              <p className='flex justify-center items-center'>Picture</p>
-            </div>
-          </div>
-          
-        </div> */}
-
-        <div className='grid grid-cols-2 w-full bg-gray-800 px-48 py-24 gap-y-24 gap-x-24'>
-          <div className='flex-col w-full h-fit col-span-1 space-y-8 p-12 '>
-            <p className='flex font-normal text-3xl justify-center'>Title like this long</p>
-            <p className='flex font-thin text-lg'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-            <p className='flex justify-end text-xl font-normal'>GitHub</p>
-          </div>
-          <div className='flex-col w-full h-[480px] col-span-1 bg-yellow-500'>
-            <p className='flex justify-center'>picture</p>
-          </div>
-
-          <div className='flex-col w-full h-[480px] col-span-1 bg-yellow-500'>
-            <p className='flex justify-center'>picture</p>
-          </div>
-          <div className='flex-col w-full h-fit col-span-1 space-y-5 p-12'>
-            <p className='flex font-normal text-3xl justify-center'>Title</p>
-            <p className='flex font-thin text-lg'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-            <p className='flex justify-end text-xl font-normal'>GitHub</p>
-          </div>
-          
-          <div className='flex-col w-full h-fit col-span-1 space-y-8 p-12 '>
-            <p className='flex font-normal text-3xl justify-center'>Title like this long</p>
-            <p className='flex font-thin text-lg'>ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-            <p className='flex justify-end text-xl font-normal'>GitHub</p>
-          </div>
-          <div className='flex-col w-full h-[480px] col-span-1 bg-yellow-500'>
-            <p className='flex justify-center'>picture</p>
-          </div>
-
         </div>
+        
 
         <div className='flex h-fit w-full bg-gray-900 p-4 justify-center space-x-1'>
           <p className='font-thin'>🖥️ Created by niekky</p>
